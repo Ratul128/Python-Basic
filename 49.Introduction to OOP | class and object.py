3888e6950fbf00cdd37d120ceb68959d4868d0dc
Ratul128 <mehediratul923@gmail.com>
2024-10-26 13:07:49 +0600

    class and object

---
+++ 49.Introduction to OOP | class and object.py
@@ -0,0 +1,16 @@
+
+class Student:
+    roll=""
+    gpa=""
+
+rahim=Student()
+
+rahim.roll=101
+rahim.gpa=3.54
+
+karim=Student()
+karim.roll=102
+karim.gpa=3.22
+
+print(f"Rahim's Roll is:{rahim.roll} Rahim's GPA is:{rahim.gpa}")
+print(f"Karim's Roll is:{karim.roll} Rahim's GPA is:{karim.gpa}")
