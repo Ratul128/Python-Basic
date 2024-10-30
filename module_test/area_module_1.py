e87a46b7797c387e1f6589e649a983681e557041
Ratul128 <mehediratul923@gmail.com>
2024-10-30 17:16:35 +0600

    import module

---
+++ module_test/area_module_1.py
@@ -0,0 +1,11 @@
+
+def triangleArea(base,height):
+    area=0.5*base*height
+    return area
+
+def ractangleArea(length,width):
+    area=length*width
+    return area
+
+#print(triangleArea(10,20))
+#print(ractangleArea(20,20))
---
+++ module_test/area_module_2.py
@@ -0,0 +1,5 @@
+
+from area_module_1 import triangleArea,ractangleArea
+
+print(triangleArea(10,20))
+print(ractangleArea(10,20))
